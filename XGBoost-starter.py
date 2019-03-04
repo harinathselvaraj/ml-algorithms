4fc7095a82ce880995b4580db3b1982600bfad6d
harinathselvaraj <harinath.selvaraj@outlook.com>
2019-03-04 14:45:49 +0000

    xgboost startcode changes + feature engineering doc

--- XGBoost-starter.py
+++ XGBoost-starter.py
@@ -23,28 +23,44 @@ x, x_test, y, y_test = train_test_split(x, y, test_size=0.33, random_state=42, s
 dtrain = xgb.DMatrix(x, label=y)
 dtest = xgb.DMatrix(x_test, label=y_test)
 evallist = [(dtest, 'eval'), (dtrain, 'train')]
-param = {'booster': 'dart',
-         'max_depth': 5,
-         'learning_rate': 0.1,
-         'objective': 'binary:logistic', 
-         'silent': True,
-         'sample_type': 'uniform',
-         'normalize_type': 'tree',
-         'rate_drop': 0.1,
-         'skip_drop': 0.5,
-         'eta': 1,
-         'objective': 'binary:logistic',
-         'nthread': 4,
-         'eval_metric': 'auc'
+
+param = {
+          'booster': 'dart', 
+          'silent':True,
+ #        'scale_pos_weight':1,
+          'learning_rate':0.01,  
+          'colsample_bytree' :0.4,
+          'subsample' : 0.8,
+          'objective':'binary:logistic', 
+          'n_estimators':100,  #100 if the size of your data is high, 1000 is if it is medium-low
+          'reg_alpha' : 0.3,
+          'max_depth':4, 
+          'gamma':10,
+          'eval_metric': 'auc'
+#          'max_depth': 4, #increase to overfit
+#          'learning_rate': 0.1,
+#          'objective': 'binary:logistic', 
+#          'silent': True,
+#          'sample_type': 'uniform',
+#          'normalize_type': 'tree',
+#          'rate_drop': 0.1,
+#          'skip_drop': 0.5,
+#          'eta': 1,
+#          'objective': 'binary:logistic',
+#          'nthread': 4,
+#          'eval_metric': 'auc'
         }
 
 # Training & Validation
 num_round = 50
 bst = xgb.train(param, dtrain, num_round, evals=evallist, early_stopping_rounds=100, verbose_eval=10)
 
-# Feature Importance Tree Plot
+# Feature Importance Tree Plot - Use them seperately
 rcParams['figure.figsize'] = 180,150
+xgb.plot_importance(bst)
 xgb.plot_tree(bst, num_trees=2)
+xgb.to_graphviz(bst, num_trees=2)
+
 
 # Prediction
 test.drop(['ID_code'], inplace=True, axis=1)
